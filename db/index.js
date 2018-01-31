1642e27005f678cd47d86ec8370bddddc7c15ce3
botzoo <rommims@gmail.com>
2018-01-30 19:25:23 -0800

    db

---
+++ db/index.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize')
+const db = new Sequelize('socialtree',
+  'socialtree', 'socialtree', {
+    host: 'localhost',
+    dialect: 'mysql'
+  })
+
+const User = db.define('user', {
+  username: { type: Sequelize.STRING, field: 'username', unique: 'username' },
+  password: Sequelize.STRING,
+  firstname: Sequelize.STRING,
+  lastname: Sequelize.STRING
+})
+
+// const TreeIcon = 'assets/0001_tree-pine.svg'
+
+// const TreeTypes = db.define('user', {
+//   icon: Sequelize.STRING,
+//   species: Sequelize.STRING,
+//   name: Sequelize.STRING,
+//   // position:   Sequelize.FLOAT,
+//     // values: ['latitude', 'longitude']
+// })
+
+User.sync()
+  // Seed thee with temp users
+  // .then(() => {
+  //   User.create({
+  //     username: 'rose',
+  //     password:'password',
+  //     firtname:'Rose',
+  //     lastname:'Rose'
+  //   })
+  //   User.create({
+  //     username: 'user2',
+  //     password:'password2',
+  //     firtname:'User2first',
+  //     lastname:'User2last'
+  //   })
+  // })
+  .then(err => {
+    console.log('Created Users Table!');
+  }, err => {
+    console.log('Error Creating Users Table:', err);
+  });
+
+exports.User = User;
--- math.js
+++
@@ -1,5 +0,0 @@
-const math = require('./node_modules_test.js')
-console.log(math)
-
-var result = math.add(2,3)
-console.log(result)
